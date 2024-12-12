6057809f098da18ba8f57ba903408af92db62d21
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-12-12 20:02:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request
-import numpy
 from transformers import pipeline
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 # Directory where your fine-tuned model and tokenizer are saved
 model_name = "SevilayG/my-bert-fake-news"
-my_model = AutoModelForSequenceClassification.from_pretrained(model_name)
-my_tokenizer = AutoTokenizer.from_pretrained(model_name)
-# Create a text classification pipeline
-clf = pipeline("text-classification", model = my_model, tokenizer = my_tokenizer)
+
+def load_pipeline():
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    return pipeline("text-classification", model=model, tokenizer=tokenizer)
+classifier = load_pipeline()
 
 # Create flask app
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder = 'template')
 
 @app.route("/", methods = ["GET"])
 def Home():
@@ -20,8 +21,8 @@ def Home():
 @app.route("/", methods = ["POST"])
 def predict():
     text = [str(x) for x in request.form.values()]
-    prediction = clf(text)
+    prediction = classifier(text)
     return render_template("index.html", prediction_text = "The news is {}".format(prediction))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
